f68d7036c329939f1f53c4233a3de2ae2bd1da00
elias.tebbani <elias.tebbani@epita.fr>
2024-04-18 19:48:18 +0200

    a lot of push, check todolist on discord (18/04/2024)

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -1,11 +1,45 @@
 import pandas as pd
 import numpy as np
-import sklearn
+import glob
 
 import timescaledb_model as tsdb
 
 db = tsdb.TimescaleStockMarketModel('bourse', 'ricou', 'db', 'monmdp')        # inside docker
 #db = tsdb.TimescaleStockMarketModel('bourse', 'ricou', 'localhost', 'monmdp') # outside docker
+
+# iterate through all files and call store_file
+def iterate_files():
+    files_2019 = glob.glob(r"boursorama/2019/*")
+    files_2020 = glob.glob(r"boursorama/2020/*")
+    files_2021 = glob.glob(r"boursorama/2021/*")
+    files_2022 = glob.glob(r"boursorama/2022/*")
+    files_2023 = glob.glob(r"boursorama/2023/*")
+    for file in files_2019:
+        name = file.split("/")[1]
+        website = file.split("/")[2]
+        print(name, website)
+        # store_file(name, website)
+    for file in files_2020:
+        name = file.split("/")[1]
+        website = file.split("/")[2]
+        print(name, website)
+        # store_file(name, website)
+    for file in files_2021:
+        name = file.split("/")[1]
+        website = file.split("/")[2]
+        print(name, website)
+        # store_file(name, website)
+    for file in files_2022:
+        name = file.split("/")[1]
+        website = file.split("/")[2]
+        print(name, website)
+        # store_file(name, website)
+    for file in files_2023:
+        name = file.split("/")[1]
+        website = file.split("/")[2]
+        print(name, website)
+        # store_file(name, website)
+    return
 
 def store_file(name, website):
     name += ".bz2"
@@ -21,9 +55,13 @@ def store_file(name, website):
             print(df.info())
             print(df.head())
         #db.df_write(df, "file_done")
-        
+
+def clean_data(df):
+    df = df.dropna()
+    df = df.drop_duplicates()
+    return df
 
 
 if __name__ == '__main__':
-    store_file("compA 2020-01-01 09:02:02.532411", "boursorama")
+    #store_file("compA 2020-01-01 09:02:02.532411", "boursorama")
     print("Done")
