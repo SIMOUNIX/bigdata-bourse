6c2de812a26e1c7f16e3d61f3eed23ce7ffb1d55
koweez <elias.tebbani@epita.fr>
2024-05-01 02:34:56 +0200

    feat: feed_companies function done but the writing in db does not do anything, feed_stocks written but not tested

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -10,14 +10,8 @@ import timescaledb_model as tsdb
 db = tsdb.TimescaleStockMarketModel('bourse', 'ricou', 'db', 'monmdp')        # inside docker
 #db = tsdb.TimescaleStockMarketModel('bourse', 'ricou', 'localhost', 'monmdp') # outside docker
 
-# iterate through all files and call store_file
 def clean_df(df):
-    # print(df.head())
-    # print(df.info())
-    # print(df.describe())
-    # print(df.shape)
     df = df.dropna()
-    #df = df.drop_duplicates() # this drops 4 million rows, so sus
     df = df.drop(columns=['symbol'])
     
     # drop rows with a 0 value in the column last
@@ -30,83 +24,83 @@ def clean_df(df):
     df['name'] = oldest_names
     return df
 
-def iterate_files(year, category='All'):
-    folders = {
-        2019: 'boursorama/2019/',
-        2020: 'boursorama/2020/',
-        2021: 'boursorama/2021/',
-        2022: 'boursorama/2022/',
-        2023: 'boursorama/2023/'
-    }
-    
-    if category == 'All':
-        file_paths = {
-            'compA': f'{folders[year]}compA*',
-            'compB': f'{folders[year]}compB*',
-            'amsterdam': f'{folders[year]}amsterdam*',
-            'peapme': f'{folders[year]}peapme*' if year >= 2021 else None
-        }
-    else:
-        file_paths = {category: f'{folders[year]}{category}*'}
-        for cat in ('compA', 'compB', 'amsterdam', 'peapme'):
-            if cat != category:
-                file_paths[cat] = None
-    
-    df_clean = {}
-    for cat, path in file_paths.items():
-        if path:
-            files = glob.glob(path)
-            if files or (category == 'peapme' and year < 2021):  # Check if files exist or if it's peapme before 2021
-                data = {}
-                for file in files:
-                    match = re.search(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+', file)
-                    if match:
-                        date_str = match.group()
-                        date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S.%f')
-                        data[date_obj] = pd.read_pickle(file)
-                
-                df = pd.concat(data)
-                df_clean[cat] = clean_df(df)
-                print(df_clean[cat].head())
-                #df_clean[cat].to_pickle(f'boursorama_clean/{cat}_{year}_clean.pkl')
-                print(f'Files of {cat} of year {year} saved dude on god skibidi')
-            else:
-                print(f'No files found for {cat}')
-                df_clean[cat] = None
-        else:
-            df_clean[cat] = None
-            
-    print('All files saved dude on god skibidi')
-    
-    return tuple(df_clean.values())
-
-market_map = map = {1 : "euronx", 2 : "lse", 3: "milano", 4: "dbx", 5: "mercados",6:"amsterdam",7:"compA",8:"compB",9:"xetra",10:"bruxelle"}
-
 def create_path_df():
+    i = 0
     # create 4 empty df with timestamp as index and 1 column being a path (string)
     #df_compA, df_compB, df_amsterdam, df_peapme = pd.DataFrame(columns=['path'], index=pd.to_datetime([])), pd.DataFrame(columns=['path'], index=pd.to_datetime([])), pd.DataFrame(columns=['path'], index=pd.to_datetime([])), pd.DataFrame(columns=['path'], index=pd.to_datetime([]))
     df_compA = pd.DataFrame(columns=['path'], index=pd.to_datetime([]))
     #files_2019, files_2020, files_2021, files_2022, files_2023 = glob.glob(f'data/2019/*'), glob.glob(f'data/2020/*'), glob.glob(f'data/2021/*'), glob.glob(f'data/2022/*'), glob.glob(f'data/2023/*')
     files_2019 = glob.glob(f'data/2019/*')
-    for files in (files_2019):#, files_2020, files_2021, files_2022, files_2023):
-        for file in files:
-            print(path)
-            path = file.split('/')[-1]
-            path = path[:-4]
-            path_without_letters  = ''.join([i for i in path if not i.isalpha()])
-            print(path_without_letters[1:-4].replace('_', ':'))
-            date = dateutil.parser.parse(path_without_letters[1:-4].replace('_', ':'))
-            if 'compA' in path:
-                df_compA.loc[date] = path
-            # elif 'compB' in path:
-            #     df_compB.loc[date] = path
-            # elif 'amsterdam' in path:
-            #     df_amsterdam.loc[date] = path
-            # elif 'peapme' in path:
-            #     df_peapme.loc[date] = path
-        return
+    for file in (files_2019):#, files_2020, files_2021, files_2022, files_2023):
+        if i == 100:
+            break
+        #for file in files:
+        path = file.split('/')[-1]
+        path = path[:-4]
+        path_without_letters  = ''.join([i for i in path if not i.isalpha()])
+        date = dateutil.parser.parse(path_without_letters)
+        if 'compA' in path:
+            df_compA.loc[date] = path
+        # elif 'compB' in path:
+        #     df_compB.loc[date] = path
+        # elif 'amsterdam' in path:
+        #     df_amsterdam.loc[date] = path
+        # elif 'peapme' in path:
+        #     df_peapme.loc[date] = path
+        i += 1
     return df_compA#, df_compB, df_amsterdam, df_peapme
+
+def feed_companies(df_compA):#, df_compB, df_amsterdam, df_peapme):
+    # companies db : id:smallint, name:varchar, market_id:smallint, symbol:varchar, symbol_nf:varchar, isin:char(12), reuters:varchar, boursorama:varchar, pea:boolean, sector:integer
+    market_map = {1 : "euronx", 2 : "lse",
+                        3: "milano", 4: "dbx",
+                        5: "mercados",6:"amsterdam",
+                        7:"compA",8:"compB",
+                        9:"xetra",10:"bruxelle"}
     
+    # getting the last day to have the last company names
+    # checked the data and all the rows are unique (no duplicates)
+    last_rows = df_compA.loc['2019-12-31']
+    df = pd.read_pickle(f'data/2019/{last_rows["path"][0]}.bz2')
+    for row in df.iterrows():
+        df = pd.DataFrame(columns=['id', 'name', 'market_id', 'symbol', 'symbol_nf', 'isin', 'reuters', 'boursorama', 'pea', 'sector'])
+        df['name'] = row[1]['name']
+        df['mid'] = 7
+        df['symbol'] = row[1]['symbol']
+        df['symbol_nf'] = None
+        df['isin'] = None
+        df['reuters'] = None
+        df['boursorama'] = None
+        df['pea'] = None
+        df['sector'] = None
+        # insertion in db does not work, idk why
+        db.df_write(df, table="companies", if_exists="append", index=False)
+        db.commit()
+
+# not tested
+def feed_stocks(df):
+    # stocks db : date: timestamptz, cid: smallint, value: float4, volume:int
+    for row in df.iterrows():
+        df = pd.DataFrame(columns=['date', 'cid', 'value', 'volume'])
+        df['date'] = row[0]
+        df['cid'] = 1
+        df['value'] = row[1]['last']
+        df['volume'] = row[1]['volume']
+        db.df_write(df, table="stocks", if_exists="append", index=False)
+        db.commit()
+        
+def feed_daystocks(df):
+    pass
+
+def file_done(path):
+    pass
+
+if __name__ == '__main__':
+    df_compA = create_path_df()
+    feed_companies(df_compA)
+    print("Done")
+
+
 # def store_file(name, website):
 #     name += ".bz2"
 #     query = tsdb.is_file_done(name)
@@ -123,21 +117,3 @@ def create_path_df():
 #             print(df.info())
 #             print(df.head())
 #         #db.df_write(df, "file_done")
-
-def feed_companies(df_compA):#, df_compB, df_amsterdam, df_peapme):
-    # get the max index of compA:
-    max_index = df_compA.index.max()
-    print(max_index)
-        
-
-
-def clean_data(df):
-    df = df.dropna()
-    df = df.drop_duplicates()
-    return df
-
-
-if __name__ == '__main__':
-    df_compA = create_path_df()
-    feed_companies(df_compA)
-    print("Done")
